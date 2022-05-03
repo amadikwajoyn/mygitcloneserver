ff3c21f0546c53f8ea173080bdae02d84a72c699
amadikwajoyn <amadikwajoyn@gmail.com>
2022-05-03 13:00:50 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+import express from "express";
+import bodyParser from "body-parser";
+import FormData from "form-data";
+import fetch from "node-fetch";
+import cors from "cors";
+import config from "./config.js";
+
+const app = express();
+
+const { client_id, redirect_uri, client_secret } = config;
+
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: "text/*" }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// Enabled Access-Control-Allow-Origin", "*" in the header so as to by-pass the CORS error.
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.post("/authenticate", (req, res) => {
+  const { code } = req.body;
+
+  const data = new FormData();
+  data.append("client_id", client_id);
+  data.append("client_secret", client_secret);
+  data.append("code", code);
+  data.append("redirect_uri", redirect_uri);
+
+  // Request to exchange code for an access token
+  fetch(`https://github.com/login/oauth/access_token`, {
+    method: "POST",
+    body: data,
+  })
+    .then((response) => {
+      return response.text();
+    })
+    .then((paramsString) => {
+      let params = new URLSearchParams(paramsString);
+      const access_token = params.get("access_token");
+      if (!access_token) {
+        throw new Error('The code passed is incorrect or expired')
+      };
+
+      // Request to return data of a user that has been authenticated
+      return fetch(`https://api.github.com/user`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      });
+    })
+    .then((response) => response.json())
+    .then((response) => {
+      return res.status(200).json(response);
+    })
+    .catch((error) => {
+      return res.status(400).json({ message: "The code passed is incorrect or expired" });
+    });
+});
+
+const PORT = process.env.SERVER_PORT || 5000;
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
